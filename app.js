5636f3584ae189543e3dd3701be7e64d1f2b8ae0
Hamza Moosa <hamzamoosa@Hamzas-MacBook-Air.local>
2022-11-18 12:33:23 +0000

    starting user signup and login system

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./database/connection");
 const app = express();
+
 const {
   getAllRestaurants,
   getRestaurantsByLocation,
@@ -10,8 +11,12 @@ const {
 const {
   getAllPreferences
 } = require("./controllers/preferenceCon");
-const { getAllUsers, getIndividualUserByUsername, patchUserByUsername } = require("./controllers/userCon");
-app.use(express.json());
+const { getAllUsers,
+   getIndividualUserByUsername,
+    patchUserByUsername,
+  postUserInfo } = require("./controllers/userCon");
+
+    app.use(express.json());
 
 app.get("/api/restaurants", getAllRestaurants);
 app.get("/api/users", getAllUsers);
@@ -21,6 +26,7 @@ app.get("/api/restaurants/:location/:name", getIndividualRestaurantByLocation);
 app.get("/api/users/:username", getIndividualUserByUsername);
 app.get("/api/preferences", getAllPreferences);
 
+app.post("/api/users", passport.authenticate(postUserInfo))
 
 app.patch("/api/users/:username", patchUserByUsername);
 
