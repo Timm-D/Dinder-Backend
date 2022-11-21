1f214620971783ec93429b892046c0c8da101e0b
liyasandeep <liyasandeep1@gmail.com>
2022-11-21 14:30:42 +0000

    changes made to host api

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const ENV = process.env.NODE_ENV || "dev";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL not set");
+}
 const config =
   ENV === "production"
     ? {
@@ -13,7 +17,7 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-const mongoString = process.env.DATABASE_URL
+const mongoString = process.env.DATABASE_URL;
 
 async function connect() {
   try {
