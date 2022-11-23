ac82bfe04583f8f398fb7d8c7f8f5d6b723944f6
timm-d <timdunstan1@gmail.com>
2022-11-23 14:05:18 +0000

    re instated Db connection

--- database/run-seed.js
+++ database/run-seed.js
@@ -4,7 +4,7 @@ const { Restaurant, Users, Preferences, seedDataBase } = require("./seed");
 const data = require("./dev_data/index");
 
 const { restaurantData, usersData, preferencesData } = data;
-
+connection();
 const runSeed = () => {
   return seedDataBase(restaurantData, usersData, preferencesData).then(() => {
     mongoose.connection.close();
