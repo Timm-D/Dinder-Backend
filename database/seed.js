f26c62f72559745bf809f2b8e196aebb795c00ca
timm-d <timdunstan1@gmail.com>
2022-11-23 12:30:15 +0000

    remove pw#

--- database/seed.js
+++ database/seed.js
@@ -71,32 +71,32 @@ const usersSchema = new mongoose.Schema({
 });
 
 
-usersSchema.pre('save', async function(next) {
-  try {
-    // check method of registration
-    const user = this;
-    if (!user.isModified('password')) next();
-    // generate salt
-    const salt = await bcrypt.genSalt(10);
-    // hash the password
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    // replace plain text password with hashed password
-    this.password = hashedPassword;
-    next();
-  } catch (error) {
-    return next(error);
-  }
-});
+// usersSchema.pre('save', async function(next) {
+//   try {
+//     // check method of registration
+//     const user = this;
+//     if (!user.isModified('password')) next();
+//     // generate salt
+//     const salt = await bcrypt.genSalt(10);
+//     // hash the password
+//     const hashedPassword = await bcrypt.hash(this.password, salt);
+//     // replace plain text password with hashed password
+//     this.password = hashedPassword;
+//     next();
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
 
 
 
-usersSchema.methods.matchPassword = async function (password) {
- try {
-   return await bcrypt.compare(password, this.password);
- } catch (error) {
-   throw new Error(error);
- }
-};
+// usersSchema.methods.matchPassword = async function (password) {
+//  try {
+//    return await bcrypt.compare(password, this.password);
+//  } catch (error) {
+//    throw new Error(error);
+//  }
+// };
 
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
